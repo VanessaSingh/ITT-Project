2ffe9df44d17d02cef761e272d841e9ece3d1c23
Soham Dongargaonkar <Soham Dongargaonkar>
2016-10-17 11:14:01 +0530

    Lots of changes; header now black, hamburger menu added, body color now black

---
+++ discover.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+    <title>Discover | Beats</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/styles.css" rel="stylesheet">
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body style="background-color: #333333">
+    <?php
+        require_once("includes/header.php");
+        require_once("includes/hamburger.php");
+    ?> 
+    <div class="container">
+
+        <!-- Discover Heading -->
+        <div>
+            <h1 class="page-header" style="color:#eeeeee">Discover
+                <small>Your mood. Your joy.</small>
+            </h1>
+        </div>
+        
+        <!-- Moods -->
+
+        <!-- Discover_Row1 -->
+        <div class="row">
+            <div class="col-md-3 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="images/party.jpg" alt="">
+                    <p class="text-under-moods">Party</p>
+                </a>
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="images/patriotic.jpg" alt="">
+                    <p class="text-under-moods">Patriotic</p>
+                </a>
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="images/romantic.jpg" alt="">
+                    <p class="text-under-moods">Romantic</p>
+                </a>
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="images/edm.png" alt="">
+                    <p class="text-under-moods">EDM</p>
+                </a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="images/folk.jpg" alt="">
+                    <p class="text-under-moods">Folk</p>
+                </a>
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="images/meditation.jpg" alt="">\
+                    <p class="text-under-moods">Meditation</p>
+                </a>
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="images/salsa.jpg" alt="">
+                    <p class="text-under-moods">Salsa</p>
+                </a>
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="images/sufi.jpg" alt="">
+                    <p class="text-under-moods">Sufi</p>
+                </a>
+            </div>
+        </div>
+
+        <div class="row">
+        <div class="col-md-3 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="images/travel.jpg" alt="">
+                    <p class="text-under-moods">Travel</p>
+                </a>
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="images/cheerful.jpg" alt="">
+                    <p class="text-under-moods">Cheerful</p>
+                </a>
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="images/dance.jpg" alt="">
+                    <p class="text-under-moods">Dance</p>
+                </a>
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="images/dreamy.jpg" alt="">
+                    <p class="text-under-moods">Dreamy</p>
+                </a>
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Beats</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
