610f14a1e14efd6005fb7c8c3768c3e122c9df98
Soham Dongargaonkar <Soham Dongargaonkar>
2016-10-21 00:05:13 +0530

    Removed everything from header

--- includes/header.php
+++ includes/header.php
@@ -5,18 +5,6 @@
 	</div>
 	<div class="menu-wrap">	
 		<nav class="menu">
-			<ul class="head">
-				<li><a href="#">Home</a></li>
-				<li><a href="#">Discover<span class="dropdown">&#9660;</span></a>
-					<ul class="sub-menu">
-						<li><a href="#">Rock</a></li>
-						<li><a href="#">Pop</a></li>
-						<li><a href="#">Old Hindi</a></li>
-					</ul>
-				</li>
-				<li><a href="#">Playlists</a></li>
-				<li><a href="#">About Us</a></li>
-			</ul>
 		</nav>
 		</header>
 	</div>
