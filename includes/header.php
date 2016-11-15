2dab6356f63bfb88436cc5289ac31e6da2f09323
Vanessa_Singh <vns.28singh@gmail.com>
2016-11-15 22:51:43 +0530

    SEARCH BAR PUSHED

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,15 @@
 <?php echo '
 <header id="headerbar">
 	<div class="logo">
+	</div>
+	<nav class="menu">
+			<div id="search-wrap" class="searchbar">
+				<form action="" autocomplete="off">
+					<input type="text" name="search" type="text" placeholder="Search for song, album, artist">
+					<input id="search-submit" value="search-submit" type="submit">
+				</form>
+			</div>
+	</nav>
+</header>
 
-	</div>
-	<div class="menu-wrap">	
-		<nav class="menu">
-		</nav>
-		</header>
-	</div>
 ';
