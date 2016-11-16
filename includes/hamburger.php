61b33a71767f2a779072fa4aaf2126d12c8ff633
Vanessa_Singh <vns.28singh@gmail.com>
2016-11-16 11:54:45 +0530

    Only hamburger fixed

--- includes/hamburger.php
+++ includes/hamburger.php
@@ -1,15 +1,15 @@
 <?php
-	echo "<div id='hamburger-menu'>
-			<ul id=hamburger-elements>
-				<li class='hamburger-li'><a href='index.php'>Home</a></li>
-				<div class='below-line'></div>
-				<li class='hamburger-li'><a href='discover.php'>Discover</a></li>
-				<div class='below-line'></div>
-				<li class='hamburger-li'><a href='playlists.php'>My Playlists</a></li>
-				<div class='below-line'></div>
-				<li class='hamburger-li'><a href='about_us.php'>About Beats</a></li>
-				<div class='below-line'></div>
-			</ul>
-		</div>
-	</div>";
+echo "<div id='hamburger-menu'>
+<ul id= 'hamburger-elements'>
+	<li class='hamburger-li'><a href='index.php'>Home</a></li>
+	<div class='below-line'></div>
+	<li class='hamburger-li'><a href='discover.php'>Discover</a></li>
+	<div class='below-line'></div>
+	<li class='hamburger-li'><a href='playlists.php'>My Playlists</a></li>
+	<div class='below-line'></div>
+	<li class='hamburger-li'><a href='about_us.php'>About Beats</a></li>
+	<div class='below-line'></div>
+</ul>
+</div>
+</div>";
 ?>
